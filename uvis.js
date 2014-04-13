f1f355b017ff544acf182e27afcd8e06eb4f52e7
Chris Chen <tech@ucdavis.edu>
2014-04-12 18:49:51 -0700

    Implemented basic data reading

--- uvis.js
+++ uvis.js
@@ -1,1 +1,17 @@
-// JavaScript Document
+// Read in the csv file
+d3.csv("unitrans-oct-2011.csv", function(d) {
+	return {
+		stoptitle: d.stopTitle,
+		date: new Date(d.date + 'T' + d.time),
+		boarding: +d.boarding,
+		deboarding: +d.deboarding,
+		route: d.route
+	};
+}, display);
+
+// Main data display function
+function display(rows) {
+	d3.select("body")
+    .append("p")
+    .text(rows[0].date);
+}
