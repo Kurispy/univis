1862a3d4853f62aeedd34aaf7c410cad950c0d1c
Chris Chen <tech@ucdavis.edu>
2014-04-12 23:09:08 -0700

    Now shows a static bar graph of the number of riders per route

--- uvis.js
+++ uvis.js
@@ -1,3 +1,12 @@
+// Width and height of the svg
+var width = 500;
+var height = 100;
+
+var svg = d3.select("body")
+	.append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
 // Read in the csv file
 d3.csv("unitrans-oct-2011.csv", function(d) {
 	return {
@@ -10,8 +19,45 @@ d3.csv("unitrans-oct-2011.csv", function(d) {
 }, display);
 
 // Main data display function
-function display(rows) {
-	d3.select("body")
-    .append("p")
-    .text(rows[0].date);
+function display(data) {
+	var byRoute = d3.nest()
+		.key(function(d) { return d.route; })
+		.sortKeys(d3.ascending)
+		.rollup(function(d) {
+			return d3.sum(d, function(b) { 
+				return b.boarding;
+			})
+		})
+		.entries(data);
+	
+	var scale = d3.scale.linear()
+		.domain([0, d3.max(byRoute, function(d) { return d.values; })])
+		.range([0, height]);
+	
+	var rects = svg.selectAll("rect")
+		.data(byRoute)
+		.enter()
+		.append("rect")
+		.attr("x", function(d, i) {
+			return i * (width / byRoute.length)
+		})
+		.attr("y", function(d) {
+			return height - scale(d.values);
+		})
+		.attr("width", width / byRoute.length - 1)
+		.attr("height", function(d) {
+			return scale(d.values);
+		})
+		.attr("fill", "cyan");
+	
+	
+
+	//d3.select("body")
+//      .selectAll("p")
+//	  .data(byRoute)
+//	  .enter()
+//	  .append("p")
+//      .text(function(d) {
+//	  	return d3.values(d);
+//	  });
 }
